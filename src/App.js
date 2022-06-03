b40f8acc2b42b5f4d3f87dd08aaed62a9efb3d6f
Shivam Rathod <rathodshivam05@gmail.com>
2022-06-03 16:27:20 +0530

    Form created

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { Form } from './components/Form';
+import { Table } from './components/Table';
+
+function App() {
+  const [data, setdata] = React.useState([])
+  const [obj, setobj] = React.useState({})
+
+  return (
+    <>
+    <h2 className='text-center my-1 text-white'>EMPLOYEE DETAILS</h2>  
+    <div className="App">
+      <Form className="left" style={{width:"25%"}} data={data} setdata={setdata} setobj={setobj}/>
+      <Table className="right" style={{width:"10%"}} data={data} setdata={setdata} key={Date.now()}/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Button } from 'react-bootstrap';
+import axios from 'axios'
+
+export const Form = (props) => {
+
+  const [userdata, setuserdata]=React.useState({
+    Name:'',
+    Age:'',
+    Address:'',
+    Depar:'',
+    Salary:'',
+    MaritalState:false,
+    Profile:'',
+  })
+
+  const inputhandle=(e)=>{
+    // console.log(e)
+    let {name, value, checked} = e.target;
+    setuserdata({
+      ...userdata, 
+      [name] : value,
+    });
+    if(name === 'MaritalState'){
+      setuserdata({
+        ...userdata, 
+        [name] : (checked),
+      });
+    }
+
+  }
+
+
+  const uloaduser=()=>{
+    axios.post('http://localhost:8080/posts/',{
+      ...userdata
+    })
+    .then((response)=> {
+    // console.log(response);
+  })
+  }
+
+  const savedata=(e)=>{
+    e.preventDefault()
+    props.data.push(userdata)
+    props.setdata(props.data)
+    props.setobj(userdata)
+    uloaduser()
+    // console.log(props.data)
+    }
+
+  return (
+    <div className="left">
+      <form action="" className="form" onSubmit={savedata}>
+        <label htmlFor="">Name</label>
+        <input type="text" name="Name" onChange={inputhandle} />
+
+        <label htmlFor="">Age</label>
+        <input type="text" name="Age" onChange={inputhandle} />
+
+        <label htmlFor="">Address</label>
+        <input type="text" name="Address" onChange={inputhandle} />
+
+        <label htmlFor="">Department</label>
+        <select name="Depar" id="" className="selectdepart" onChange={inputhandle}>
+          <option value="">Select Department</option>
+          <option value="Math">Mathmatics</option>
+          <option value="Che">Chemistry</option>
+          <option value="Phy">Physics</option>
+          <option value="Bio">Biology</option>
+        </select>
+        {/* <input type="text" name="Department" /> */}
+
+        <label htmlFor="">Salary</label>
+        <input type="text" name="Salary" onChange={inputhandle} />
+
+        <label htmlFor="">Martial Status</label>
+        <input type="checkbox" className="checkboxui"  name="MaritalState" onChange={inputhandle}/>
+
+        <label htmlFor="">Profile Picture Link</label>
+        <input type="text" name="Profile" onChange={inputhandle}/>
+
+        <Button type="submit">Save</Button>
+      </form>
+      </div>
+  );
+};
